b13fb4ab65193caf452abb8467db805149c6d656
Mike Stamm <mike@heatlossgames.com>
2024-09-13 17:42:48 -0400

    New grid home page, solo comic pages.

--- .eleventy.js
+++ .eleventy.js
@@ -88,25 +88,7 @@ export default function(eleventyConfig) {
      - Read the Next Steps post to learn how to extend this
   */
   eleventyConfig.addCollection("comics", function(collection) {
-
-    /* The posts collection includes all posts that list 'posts' in the front matter 'tags'
-       - https://www.11ty.dev/docs/collections/
-    */
-
-    // EDIT HERE WITH THE CODE FROM THE NEXT STEPS PAGE TO REVERSE CHRONOLOGICAL ORDER
-    // (inspired by https://github.com/11ty/eleventy/issues/898#issuecomment-581738415)
-    const coll = collection
-      .getFilteredByTag("comics");
-
-    // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426 
-    // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
-    for (let i = 0; i < coll.length; i++) {
-      const prevPost = coll[i - 1];
-      const nextPost = coll[i + 1];
-
-      coll[i].data["prevPost"] = prevPost;
-      coll[i].data["nextPost"] = nextPost;
-    }
+    const coll = collection.getFilteredByTag("comics");
 
     return coll;
   });
---
+++ src/comics/lift.md
@@ -0,0 +1,11 @@
+---
+title: LIFT (Living in Future Tense)
+credits: Suzana Harcum and Owena White
+site: https://www.liftcomic.com/
+thumbnail: https://webcomictraveloguecms.wordpress.com/wp-content/uploads/2024/07/livinginfuturetenseicon.png
+tags:
+  - comics
+layout: layouts/comic.njk
+---
+
+A sequel to Tripping Over You. Milo and Liam have settled into their long-term careers and begun a family. Their lives are uprooted, however, when Milo's baby sister needs a quiet place to heal from a traumatic accident.
